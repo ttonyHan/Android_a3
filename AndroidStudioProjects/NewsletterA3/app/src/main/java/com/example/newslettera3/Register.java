5f6656e7a89313255da1b8f0b1875abbbbf1d132
ttonyHan <ttonytony774@gmail.com>
2023-01-03 13:51:58 +0700

    basic login,register

--- AndroidStudioProjects/NewsletterA3/app/src/main/java/com/example/newslettera3/Register.java
+++ AndroidStudioProjects/NewsletterA3/app/src/main/java/com/example/newslettera3/Register.java
@@ -1,37 +1,75 @@
 package com.example.newslettera3;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
-    TextInputLayout editTextEmail, editTextPassword,editTextFullname;
+    TextInputEditText editTextEmail, editTextPassword, editTextFullname;
     Button btn_register;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        mAuth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         editTextFullname = findViewById(R.id.fullname);
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.loginNow);
+        btn_register = findViewById(R.id.btn_register);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
         //onclick listener for btn
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullname,email,password;
-                fullname = String.valueOf(editTextFullname.getEditText());
-                email = String.valueOf(editTextEmail.getEditText());
-                password = String.valueOf(editTextPassword.getEditText());
+                progressBar.setVisibility(View.VISIBLE);
+                String email,password,fullname;
+                fullname = String.valueOf(editTextFullname.getText());
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
 
                 if(TextUtils.isEmpty(fullname)){
                     Toast.makeText(Register.this,"Enter your full name",Toast.LENGTH_SHORT).show();;
@@ -45,12 +83,26 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this,"Enter your password",Toast.LENGTH_SHORT).show();;
                     return ;
                 }
+
+                //create user from fb
+                //https://firebase.google.com/docs/auth/android/password-auth
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Register.this, "Login successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
-
-            //create user from fb
-            //https://firebase.google.com/docs/auth/android/password-auth
-
-
         });
 
     }
